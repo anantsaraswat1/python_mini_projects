62b7af31ef20fc63cce9eacd9ecfe12311c16692
Prasun Mittal <prasunmittal09@gmail.com>
2024-04-26 00:32:09 +0530

    new

--- inventary.py
+++ inventary.py
@@ -1,16 +1,16 @@
 import pandas as pd
 l={'Banana':50,'Apple':200,'Kiwi':300,'Papaya':150,'Orange':250,'Imli':400,'Guava':100,'maggi':14,'milk':20,'dahi':40}
 print(pd.Series(l))
-print('\n')
 a={}
 c=n=0
-print('\n')
 for i in l:
-    print(i,l[i])
+    print('\n',i,l[i])
     k=int(input('enter weight in kg: '))
     a[i]=[l[i],k]
     c+=k
     n+=k*l[i]
-print('\n\n',pd.DataFrame(a,index=['price','Qty']))
+reciept=pd.DataFrame(a,index=['price','Qty']).T
+reciept['Total']=reciept['price']*reciept['Qty']
+print('\n\n',reciept)
 print('\ntotal weight= ',c)
-print('total price= ',n)
+print('total Price= ',n)
